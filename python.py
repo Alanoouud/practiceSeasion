3eedd41002eed635de86721d14c90ddb28150b37
Alanood Almatham <alanoodalmatham@Alanoods-MacBook-Pro.local>
2024-09-28 17:14:29 +0300

    hello

---
+++ python.py
@@ -0,0 +1,8 @@
+print("hello world, lets practice python")
+num = 1
+name = input("enter your name in small letters: ")
+if (name='anoud'):
+    print("your name is the same as me!")
+else:
+    print(name," is great name!")
+print(f"hello, {input("nameagain: ")}")
